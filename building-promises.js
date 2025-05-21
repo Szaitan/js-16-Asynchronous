8f8d2fb70b53323c7176aef4644ca212dbc5081f
Michal <m.krepiniewicz@gmail.com>
2025-05-21 20:23:54 +0200

    Building promises -rev.02

--- building-promises.js
+++ building-promises.js
@@ -1,13 +1,20 @@
 'use strict';
 
 //////////////////////////////////
+// By creating promise we have to pass executor(callback) function
+// which takes two parameters resolve and reject
+// Resolve parameter is used when the statment is fullfieled
+// Reject when its not
+
 const lotteryPromise = new Promise(function (resolve, reject) {
   console.log('Lottery draw is happening !!!');
-  if (Math.random() >= 0.5) {
-    resolve('YOU win !!!');
-  } else {
-    reject(new Error('YOU lost !!!'));
-  }
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('YOU win !!!');
+    } else {
+      reject(new Error('YOU lost !!!'));
+    }
+  }, 2000);
 });
 
 lotteryPromise
